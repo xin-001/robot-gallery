5a5b006de6387931041a2d8f4455aeb764d99098
Xin Ma <xinma.mba@outlook.com>
2022-10-06 18:45:57 -0500

    project completed

--- src/components/Robot.tsx
+++ src/components/Robot.tsx
@@ -1,5 +1,7 @@
-import React from "react";
-import styles from './Robot.module.css'
+import React,{useContext} from "react";
+import styles from './Robot.module.css';
+import {appContext,appSetStateContext} from "../AppState";
+
 
 interface RobotProps{
     id: number;
@@ -8,11 +10,27 @@ interface RobotProps{
 }
 
 const Robot : React.FC<RobotProps> = ({id,name,email}) => {
+    const value = useContext(appContext);
+    const setState = useContext(appSetStateContext);
+    const addToCart = () =>{
+        if(setState){
+            setState(state => {
+                return {
+                    ...state,
+                    shoppingCart:{
+                        items: [...state.shoppingCart.items,{id,name}]
+                    }
+                }
+            })
+        }
+    }
     return (
     <div className={styles.cardContainer}>
         <img alt="robot" src={`https://robohash.org/${id}`}/>
         <h2>{name}</h2>
         <p>{email}</p>
+        <p>author:{value.username}</p>
+        <button onClick={addToCart}>add to Cart</button>
     </div>)
 };
 
